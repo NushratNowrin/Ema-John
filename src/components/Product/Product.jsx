96ac3b4c5272ae9b3dce153c5463408299d595c6
NushratNowrin <nowrin2023@gmail.com>
2023-04-01 01:54:29 +0600

    Sidebar added

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -1,22 +1,34 @@
-import React from 'react';
-import './Product.css'
+import React from "react";
+import "./Product.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faShoppingCart } from "@fortawesome/free-solid-svg-icons";
 
-const Product = ({img, name, price, seller, ratings}) => {
-    return (
-        <div className='product'>
-        <div className='single-product'>
-            <img src={img} alt="" />
-            <div className="product-details">
-                <p className='product-name'>{name}</p>
-                <p className='product-price'>Price :${price}</p>
-                <br />
-                <p><small>Manufacturer: {seller}</small></p>
-                <p><small>Rating: {ratings} star</small></p>
-            </div>
-        </div>
-        <button className='add-to-cart-btn'>Add to Cart</button>
-        </div>
-    );
+const Product = (props) => {
+	const { img, name, price, seller, ratings } = props.product;
+	const handleCart = props.handleCart;
+
+	return (
+		<div className='product'>
+			<div className='single-product'>
+				<img src={img} alt='' />
+				<div className='product-details'>
+					<p className='product-name'>{name}</p>
+					<p className='product-price'>Price :${price}</p>
+					<p>
+						<small>Manufacturer: {seller}</small>
+					</p>
+					<p>
+						<small>Rating: {ratings} star</small>
+					</p>
+				</div>
+			</div>
+			<button
+				className='add-to-cart-btn'
+				onClick={() => handleCart(props.product)}>
+				Add to Cart <FontAwesomeIcon icon={faShoppingCart} />
+			</button>
+		</div>
+	);
 };
 
 export default Product;
--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -3,12 +3,37 @@ import Product from '../Product/Product';
 import './Shop.css';
 
 const Shop = () => {
+    // const eventHandler = (product) =>{
+    //     eventHandler(product);
+    // }
     const [products, setProducts] = useState([]);
+    const [carts, setCarts] = useState([]);
+    const [price, setPrice] = useState(0);
+    let shippingCharge;
     useEffect(() => {
         fetch('https://raw.githubusercontent.com/ProgrammingHero1/ema-john-resources/main/fakeData/products.json')
         .then(res => res.json())
         .then(data => setProducts(data))
     } ,[])
+    const handleCart = (product) =>{
+        let newCart = [...carts, product];
+        setCarts(newCart);
+        const productPrice = product.price;
+        const newPrice = price+ productPrice;
+        setPrice(newPrice);
+    }
+    if(price == 0){
+        shippingCharge = 0;
+    }
+    else if(price < 500){
+        shippingCharge = 20;
+    }
+    else if(price>500 && price<1000){ 
+        shippingCharge = 10;
+    }
+    else{
+        shippingCharge = 5;
+    }
     return (
         <div className='shop-container'>
             <div className='products-container'>
@@ -16,13 +41,19 @@ const Shop = () => {
                 products.map(product => 
                 <Product
                     key={product.id}
-                    {...product}
+                    product = {product}
+                    handleCart = {handleCart}
                 >
                 </Product>)
                 }
             </div>
             <div className="side-bar">
-                <p>sidebar</p>
+                <h2>Order Summary</h2>
+                <p>Selected Items: {carts.length}</p>
+                <p>Total Price: ${price}</p>
+                <p>Total Shipping Charge: ${shippingCharge}</p>
+                <p>Tax: $114</p>
+                <h3>Grand Total: ${price + shippingCharge + 114}</h3>
             </div>
         </div>
     );
